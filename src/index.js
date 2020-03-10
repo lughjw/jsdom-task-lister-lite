7bf76cb33c7a4acdadb4778bf5811c892df90a41
lughjw <lughjw@gmail.com>
2020-03-10 12:07:35 -0500

    finished basic requirements

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
+
+const taskArray = [];
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.getElementById("create-task-form");
+  // console.log("Dom loaded");
+  taskForm.addEventListener("submit", addToTaskList);
 });
+
+function addToTaskList(event) {
+  event.preventDefault(); 
+  // console.log(event);
+  let text = document.getElementById("new-task-description").value;
+  taskArray.push(text);
+  // console.log(taskArray);
+
+  displayList();
+}
+
+function displayList(){
+  const taskList = document.getElementById("tasks");
+  // taskList.innerHTML = ""; // equivalent to the below, but slightly slower which doesn't matter.
+  while(taskList.hasChildNodes()) {
+    taskList.childNodes[0].remove();
+  }
+
+  for(let i=0; i < taskArray.length; i++) {
+    let li = document.createElement("li");
+    li.innerText = taskArray[i];
+    taskList.appendChild(li);
+  }
+}
